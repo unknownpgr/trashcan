aabc55ad9ada8f3ecb227681c272e38c52ea03a8
powergee <49356944+powergee@users.noreply.github.com>
2019-12-19 19:59:52 +0900

    Added codes to test WebInteract (1.4)

--- Algorithm/ported_C++/Headers/WebInteract.h
+++ Algorithm/ported_C++/Headers/WebInteract.h
@@ -6,14 +6,17 @@
 #include <memory>
 using namespace std::filesystem;
 
-enum class CommFromWeb { QueryPath = '@', Undefined = '\0' };
-typedef std::pair<CommFromWeb, std::vector<std::string>> DataFromWeb;
-typedef std::shared_ptr<DataFromWeb> DataFromWebPtr;
+namespace WebInteract
+{
+	enum class CommFromWeb { Calibrate, Explore, GetStatus, Go, Undefined };
+	typedef std::pair<CommFromWeb, std::vector<std::string>> DataFromWeb;
+	typedef std::shared_ptr<DataFromWeb> DataFromWebPtr;
 
-const path Path_ToSend = "C:\\WebInteractTest\\Receive";
-const path Path_ToReceive = "C:\\WebInteractTest\\Receive";
-// 웹으로부터 오는 명령들의 종류를 정의
+	const path Path_ToSend = "C:\\WebInteractTest\\Send";
+	const path Path_ToReceive = "C:\\WebInteractTest\\Receive";
+	// 웹으로부터 오는 명령들의 종류를 정의
 
-void SendData(const std::string& fileName, const std::string& content);
-// 웹으로부터 온 데이터(파일)를 확인. 처리할 명령이 없다면 Undefined를 반환.
-DataFromWebPtr ReceiveData();
+	void SendData(const std::string& fileName, const std::string& content);
+	// 웹으로부터 온 데이터(파일)를 확인. 처리할 명령이 없다면 Undefined를 반환.
+	DataFromWebPtr ReceiveData();
+}
